3026e13bdfc2208419cebaffda20e0a819ededc7
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-06-25 16:30:23 +0200

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,52 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+
+    const posts = useStaticQuery( graphql`
+        query {
+            allWordpressWpRadioposts {
+                edges {
+                    node {
+                        title
+                        slug
+                        date
+                    }
+                }
+            }
+        }
+    `)
+
+
+    return (
+
+        <Layout>
+            {/*<Head title = "Posts" />*/}
+            <h1>A result of Phantom Radio Archive Posts.</h1>
+            <ul>
+                {
+                    posts.allWordpressWpRadioposts.edges.map((edge) => {
+                        return (
+                            <li>
+                                <Link to={edge.node.slug}>
+                                    <h2>{edge.node.title}</h2>
+
+                                    <p>{edge.node.date}</p>
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </Layout>
+    )
+
+}
 
 export default IndexPage
+
+
